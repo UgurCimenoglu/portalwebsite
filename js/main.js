23c12207a9f70c4a978d033d9b261c28a70d9631
Ugur Cimen <ugurcimenogluu@gmail.com>
2020-12-09 14:21:57 +0300

    oyun portal

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    let owl = $(".owl-carousel").owlCarousel({
+        autoplay : true,
+        dots : false,
+        items : 1,
+        loop:true,
+        autoplayHoverPause:true,
+        autoplaySpeed:1000,
+        
+    })
+    });
+
